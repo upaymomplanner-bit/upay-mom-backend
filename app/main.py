e9e815d65a0430f246124cb2ee5cbc4afef5c2c3
prajasnaik <prajadnaik@gmail.com>
2025-12-13 17:11:55 +0530

    uv fix, re added auth

--- app/main.py
+++ app/main.py
@@ -14,23 +14,23 @@ async def root():
     return {"message": "Welcome to UPay MOM Backend"}
 
 
-# @app.middleware("http")
-# async def supabase_auth_middleware(
-#     request: Request,
-#     call_next,
-# ):
-#     if request.method == "OPTIONS":
-#         return await call_next(request)
+@app.middleware("http")
+async def supabase_auth_middleware(
+    request: Request,
+    call_next,
+):
+    if request.method == "OPTIONS":
+        return await call_next(request)
 
-#     auth_header = request.headers.get("Authorization")
-#     if not auth_header or not auth_header.startswith("Bearer "):
-#         raise HTTPException(status_code=401, detail="Unauthorized")
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
-#     jwt_token = auth_header.split(" ")[1]
-#     supabase = await get_supabase_client()
-#     user = await supabase.auth.get_user(jwt_token)
-#     if not user or not user.user:
-#         raise HTTPException(status_code=401, detail="Unauthorized")
+    jwt_token = auth_header.split(" ")[1]
+    supabase = await get_supabase_client()
+    user = await supabase.auth.get_user(jwt_token)
+    if not user or not user.user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
-#     response = await call_next(request)
-#     return response
+    response = await call_next(request)
+    return response
